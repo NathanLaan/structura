6e399c47f6c3fcb3109e27b790eb47c73241770e
nathanlaan <722624+NathanLaan@users.noreply.github.com>
2025-06-30 16:17:53 -0600

    docs: Code docs cleanup

--- structura-lib/src/container/mod.rs
+++ structura-lib/src/container/mod.rs
@@ -20,85 +20,15 @@ pub trait Container {
     fn update_layout(&mut self);
 }
 
+///
+/// Combination `Container` + `Component` trait.
+///
 pub trait ContainerComponent: Container + Component {}
 impl<T> ContainerComponent for T where T: Container + Component {}
 
-// impl Container {
-//     pub fn new() -> Self {
-//         Self { children: vec![] }
-//     }
-//
-//     pub fn push<C: Component + 'static>(&mut self, component: C) {
-//         self.children.push(Box::new(component));
-//     }
-// }
-
-// impl Component for Container {
-//     fn update(&mut self, input: MouseInput) {
-//         for child in self.children.iter_mut() {
-//             child.update(input);
-//         }
-//     }
-//
-//     fn draw(&self, context: &mut BufferContext) {
-//         for child in self.children.iter() {
-//             child.draw(context);
-//         }
-//     }
-//
-//     fn set_position(&mut self, x: f64, y: f64) {
-//         todo!()
-//     }
-//
-//     fn get_position(&self) -> Point {
-//         //
-//         // TODO: Should be minimum position?
-//         //
-//         if self.children.is_empty() {
-//             Point { x: 0.0, y: 0.0 }
-//         } else {
-//             Point {
-//                 x: self.children[0].get_position().x,
-//                 y: self.children[0].get_position().y,
-//             }
-//         }
-//     }
-//
-//     fn set_size(&mut self, width: usize, height: usize) {
-//         todo!()
-//     }
-//
-//     ///
-//     /// Returns the size of the `Container`.
-//     ///
-//     /// The size is defined as the bounding box of all child controls.
-//     ///
-//     fn get_size(&self) -> Size {
-//         if self.children.is_empty() {
-//             return Size {
-//                 width: 0,
-//                 height: 0,
-//             };
-//         }
-//         let mut min_x = self.children[0].get_position().x;
-//         let mut min_y = self.children[0].get_position().y;
-//         let mut max_x = min_x + self.children[0].get_size().width as f64;
-//         let mut max_y = min_y + self.children[0].get_size().height as f64;
-//         for component in self.children.iter().skip(1) {
-//             min_x = min_x.min(component.get_position().x);
-//             min_y = min_y.min(component.get_position().y);
-//             max_x = max_x.max(component.get_position().x + component.get_size().width as f64);
-//             max_y = max_y.max(component.get_position().y + component.get_size().height as f64);
-//         }
-//         let max_w = (max_x - min_x).ceil() as u32;
-//         let max_h = (max_y - min_y).ceil() as u32;
-//         Size {
-//             width: max_w,
-//             height: max_h,
-//         }
-//     }
-// }
-
+///
+/// Horizontal Row of components.
+///
 pub struct Row {
     pub children: Vec<Box<dyn Component>>,
     pub spacing: usize,
@@ -109,7 +39,7 @@ pub struct Row {
 
 impl Row {
     ///
-    /// Constructor
+    /// Constructor.
     ///
     pub fn new(x: f64, y: f64, spacing: usize, height: usize) -> Self {
         Self {
